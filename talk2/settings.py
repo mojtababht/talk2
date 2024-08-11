1b822639e0247bbee1f70cc1929400461f3317e7
mojtaba <mojtababht@gmail.com>
2024-08-11 10:12:39 +0330

    added user model it will change

--- talk2/settings.py
+++ talk2/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third_party
+    'rest_framework',
+
+    # internal
+    'chats',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -112,6 +119,11 @@ USE_I18N = True
 USE_TZ = True
 
 
+REST_FRAMEWORK = {
+
+}
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
@@ -121,3 +133,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
+from uuid import uuid4
+
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(UserManager):
+    ...
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid4, unique=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
